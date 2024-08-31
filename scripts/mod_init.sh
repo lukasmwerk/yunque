6299ec7012f5a0be7fbf61d94fe3578678ae21bd
lukas <lukasmwerk@gmail.com>
2024-08-30 20:07:58 -0400

    might not need this script, different go mod structure

--- scripts/mod_init.sh
+++ scripts/mod_init.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+# #!/bin/bash
 
-cd "$(dirname "$0")/.."
+# cd "$(dirname "$0")/.."
 
-for service in services/*; do
-    if [ -d "$service" ]; then
-        cd $service
-        # Check if valid go dir
-        if ls *.go 1> /dev/null 2>&1; then
-            go mod init $(basename $service)
-            go mod tidy
-        else
-            echo "Skipping $service: No Go files found"
-        fi
-        cd - >/dev/null
-    fi
-done
+# for service in services/*; do
+#     if [ -d "$service" ]; then
+#         cd $service
+#         # Check if valid go dir
+#         if ls *.go 1> /dev/null 2>&1; then
+#             go mod init $(basename $service)
+#             go mod tidy
+#         else
+#             echo "Skipping $service: No Go files found"
+#         fi
+#         cd - >/dev/null
+#     fi
+# done
