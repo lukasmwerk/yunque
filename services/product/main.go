2236e1e7b5fe78962b50942b67bb40d8452d0492
lukas <lukasmwerk@gmail.com>
2024-08-31 11:19:38 -0400

    Minor server and logging changes

--- services/product/main.go
+++ services/product/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"os"
+
+	"github.com/lukasmwerk/yunque/libs/logger"
+	"github.com/lukasmwerk/yunque/services/product/server"
+)
+
+func main() {
+	logger := logger.NewLogger(0)
+	logger.UpdateStatus("Starting product service server...")
+	session := server.NewSession(logger)
+	logger.UpdateStatus("Running product service server...")
+	session.Run() // enter run loop
+	logger.UpdateStatus("Shutting down gracefully...")
+
+	os.Exit(session.ExitCode)
+}
