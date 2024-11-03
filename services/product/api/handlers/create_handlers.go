294849295dce83c37134e2cb8add9eeeddd2cb0c
Lukas Werk <lukasmwerk>
2024-11-03 10:58:57 -0500

    Add unversioned

---
+++ services/product/api/handlers/create_handlers.go
@@ -0,0 +1,15 @@
+package api
+
+// handleAddNewProduct handles a create product operation
+// This operation does not fail if the product does not exist
+// since the database will create a new surrogate key regardless of
+// the product details.
+func handleAddNewProduct() {
+
+}
+
+// handleAddExistingProduct handles a create existing operation
+// This operation creates a new product from an existing product
+func handleAddExistingProduct() {
+
+}
