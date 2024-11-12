c9a0dd862c38fe5eb745e3a2a47edbea8ab16c3a
Lukas Werk <lukasmwerk@gmail.com>
2024-11-12 14:07:07 -0500

    Create session type

---
+++ services/shop/models/session.go
@@ -0,0 +1,9 @@
+package models
+
+import "github.com/lukasmwerk/yunque/libraries/types"
+
+// Session represents an open session, and is used to
+type Session struct {
+	SessionID types.ID // (for debugging)
+
+}
