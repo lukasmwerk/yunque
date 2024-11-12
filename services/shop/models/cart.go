6d7a34dcd0d0fb9b3eb4f4b2a387a2bc75531603
Lukas Werk <lukasmwerk@gmail.com>
2024-11-12 14:06:24 -0500

    Named item struct

--- services/shop/models/cart.go
+++ services/shop/models/cart.go
@@ -5,22 +5,30 @@ import (
 	"time"
 )
 
+// Item is a product representation in a cart.
+// It holds the amount, and a limited subset of product fields.
+type Item struct {
+	// Amount of this item in the cart.
+	Amount int
+
+	// ProductID of the product that this item is.
+	ProductID types.ID
+
+	ProductName  string      // (for optimization)
+	ProductPrice types.Price // (for optimization)
+}
+
 type Cart struct {
 	// UserID is a unique identifier of the user that owns this cart.
 	UserID types.ID
 
-	// Items are products realized in a cart.
-	Items []struct {
-		Amount       int // Amount of this item in the cart
-		ProductID    types.ID
-		ProductName  string      // (optimization)
-		ProductPrice types.Price // (optimization)
-	}
+	// All the items currently in the Cart.
+	Items []Item
 
 	// Subtotal is the summed price of all the items in the cart,
 	// and does not include deals, coupons or fees.
 	Subtotal int
 
-	// LastSnap is the last time this cart was snapshot to the persistent store
+	// LastSnap is the last time this cart was snapshot to the persistent store.
 	LastSnap time.Time
 }
