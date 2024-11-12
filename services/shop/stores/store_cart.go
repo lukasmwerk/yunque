69b60da4ff90b37d701af96feea251cc69fdfe72
Lukas Werk <lukasmwerk@gmail.com>
2024-11-12 14:07:50 -0500

    Init stores and basic functions

--- services/shop/stores/store_cart.go
+++ services/shop/stores/store_cart.go
@@ -1,3 +1,33 @@
 package stores
 
-// Database implementation for carts
+import (
+	"context"
+	"github.com/lukasmwerk/yunque/libraries/types"
+	"github.com/lukasmwerk/yunque/services/shop/models"
+)
+
+// Database implementation for carts.
+
+func CacheProductInCart(ctx context.Context, db *Session, userID types.ID, productID types.ID) error {
+	return nil
+}
+
+func RemoveProductFromCart(ctx context.Context, db *Session, userID types.ID, productID types.ID) error {
+	return nil
+}
+
+func SetProductAmountInCart(ctx context.Context, db *Session, userID types.ID, productID types.ID, amount int) error {
+	return nil
+}
+
+func SnapshotCart(ctx context.Context, db *Session, userID types.ID) error {
+	return nil
+}
+
+func ClearUserCart(ctx context.Context, db *Session, id types.ID) error {
+	return nil
+}
+
+func GetCart(ctx context.Context, db *Session, id types.ID) (*models.Cart, error) {
+	return nil, nil
+}
---
+++ services/shop/stores/store_product.go
@@ -0,0 +1,10 @@
+package stores
+
+import (
+	"context"
+	"github.com/lukasmwerk/yunque/libraries/types"
+)
+
+func GetProductListing(ctx context.Context, db *Session, id types.ID) error {
+	return nil
+}
