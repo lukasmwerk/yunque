d59c1770194bdaf893fd9cf6997772f0a68706cb
Lukas Werk <lukasmwerk@gmail.com>
2024-11-12 14:05:48 -0500

    Added product getter

--- services/shop/handlers/handle_product.go
+++ services/shop/handlers/handle_product.go
@@ -1,1 +1,12 @@
 package handlers
+
+import (
+	"context"
+	"github.com/lukasmwerk/yunque/libraries/types"
+	"github.com/lukasmwerk/yunque/services/shop/stores"
+)
+
+// GetProductListing returns the listing data for a product.
+func GetProductListing(ctx context.Context, db *stores.Session, productID types.ID) error {
+	return stores.GetProductListing(ctx, db, productID)
+}
