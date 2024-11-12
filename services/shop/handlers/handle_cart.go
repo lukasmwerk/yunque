e07dc19687bfe7e1028ad2bf2643bf2002de017d
Lukas Werk <lukasmwerk@gmail.com>
2024-11-12 14:05:11 -0500

    Added database calls to cart handler

--- services/shop/handlers/handle_cart.go
+++ services/shop/handlers/handle_cart.go
@@ -2,40 +2,46 @@ package handlers
 
 import (
 	"context"
+	"errors"
 	"github.com/lukasmwerk/yunque/libraries/types"
+	"github.com/lukasmwerk/yunque/services/shop/stores"
 )
 
 // CacheProductInCart preemptively inserts a product into the user's cart with a frequency of zero.
 // The inserted product is stored in a map of the cart, and if a user chooses to add an amount of
 // the item to their cart, the amount for key productID is simply updated.
-func CacheProductInCart(userID types.ID, productID types.ID, cxt context.Context) error {
-	return nil
+func CacheProductInCart(ctx context.Context, db *stores.Session, userID types.ID, productID types.ID) error {
+	return stores.CacheProductInCart(ctx, db, userID, productID)
 }
 
 // RemoveProductFromCart deletes a product from the user cart, both in the cache and persistent store.
-func RemoveProductFromCart(userID types.ID, productID types.ID, cxt context.Context) error {
-	// TODO: delete from cache
-	err := SnapshotCart(cxt)
-	return err
+func RemoveProductFromCart(ctx context.Context, db *stores.Session, userID types.ID, productID types.ID) error {
+	return stores.RemoveProductFromCart(ctx, db, userID, productID)
 }
 
 // SetProductAmountInCart updates the user's cart with an amount of the passed product.
-func SetProductAmountInCart(userID types.ID, productID types.ID, amount int, cxt context.Context) error {
-	defer SnapshotCart(cxt) // should fail quietly
-	return nil
+func SetProductAmountInCart(ctx context.Context, db *stores.Session, userID types.ID, productID types.ID, amount int) error {
+	defer SnapshotCart(ctx, db, userID) // should fail quietly
+	return stores.SetProductAmountInCart(ctx, db, userID, productID, amount)
 }
 
-// SnapshotCart saves a snapshot of the user's cart to the persistent store.
-func SnapshotCart(cxt context.Context) error {
-	return nil
+// SnapshotCart saves a snapshot of the user's cart (over the network) to the persistent store.
+func SnapshotCart(ctx context.Context, db *stores.Session, userID types.ID) error {
+	return stores.SnapshotCart(ctx, db, userID)
 }
 
 // ClearUserCart removes all products from a user's cart. No undo.
-func ClearUserCart(userID types.ID, cxt context.Context) error {
-	return nil
+func ClearUserCart(ctx context.Context, db *stores.Session, userID types.ID) error {
+	return stores.ClearUserCart(ctx, db, userID)
 }
 
 // ViewCart retrieves the cart for a user.
-func ViewCart(userID types.ID, cxt context.Context) error {
+func ViewCart(ctx context.Context, db *stores.Session, userID types.ID) error {
+	cart, err := stores.GetCart(ctx, db, userID) // TODO do something with this cart
+	if err != nil {
+		return err
+	} else if cart == nil {
+		return errors.New("") // TODO add error
+	}
 	return nil
 }
